2dec21a73d5170287fc1c691f118200c9a124742
Neil Martin <87085416+neilmartindev@users.noreply.github.com>
2022-05-05 11:18:07 +0100

    Update ubuntuSetup.sh

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-apt-get update  # To get the latest package lists
-apt-get upgrade -y
-apt-get install steam discord snap git code python3.8 libreoffice kdenlive foliate caffeine flameshot vlc gnome-tweaks neofetch qbittorrent -y
+LIST_OF_APPS="steam discord snap git code python3.8 libreoffice kdenlive foliate caffeine flameshot vlc gnome-tweaks neofetch qbittorrent"
+
+apt update
+apt install -y $LIST_OF_APPS
