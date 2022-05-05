6f86ad9f8cf7621857dc660a9cb02652e45d8ee2
Neil Martin <87085416+neilmartindev@users.noreply.github.com>
2022-05-05 11:12:09 +0100

    Create ubuntuSetup.sh

---
+++ ubuntuSetup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+apt-get update  # To get the latest package lists
+apt-get upgrade -y
+apt-get install steam discord snap git code python3.8 libreoffice kdenlive foliate caffeine flameshot vlc gnome-tweaks neofetch qbittorrent -y
